ed85e6618f089eef5737ea23644cc9369ff01b7f
Jennifer Villa <jvilla2013@gmail.com>
2018-11-16 11:06:38 -0800

    Adding more plotting code to SVM for error analysis.

--- SVM_base.py
+++ SVM_base.py
@@ -86,10 +86,61 @@ print("RBF Performance on test set: " + str(test_perf_rbf) )
 Y_train_pred = clf.predict(X_train)
 train_perf = accuracy_score(Y_train, Y_train_pred)
 
+#Lets look at how far we are off
+dif_matrix = Y_train - Y_train_pred
+unique, counts = np.unique(dif_matrix, return_counts=True)
+my_class_counts = dict(zip(unique, counts))
+
+dif_matrix_abs = np.abs(dif_matrix)
+unique, counts = np.unique(dif_matrix_abs, return_counts=True)
+my_class_counts_abs = dict(zip(unique, counts))
+
+x_count = [int(v) for v in my_class_counts.keys()]
+y_count = [my_class_counts[v] for v in my_class_counts.keys()]
+
+x_count_abs = [int(v) for v in my_class_counts_abs.keys()]
+y_count_abs = [my_class_counts_abs[v] for v in my_class_counts_abs.keys()]
+
+plt.clf()
+plt.bar(x_count,y_count)
+plt.xlabel('Error Misclassification')
+plt.ylabel('Number of Occurences')
+plt.title('Number of Buckets SVM Was Off By')
+plt.show()
+
+plt.clf()
+plt.bar(x_count_abs,y_count_abs)
+plt.xlabel('Magnitude of Error Misclassification')
+plt.ylabel('Number of Occurences')
+plt.title('Number of Buckets SVM Was Off By')
+plt.show()
+
+#total records
+total_rec = sum(y_count_abs)
+zero_to_one_bucket = sum(y_count_abs[0:2])
+print("Percentage off by 1 bucket or less: " + str( zero_to_one_bucket / total_rec) )
+
+##Perform same analysis for test set 
+dif_matrix_abs_test = np.abs(Y_test - Y_test_pred)
+unique, counts = np.unique(dif_matrix_abs_test, return_counts=True)
+
+plt.clf()
+plt.bar(unique,counts)
+plt.xlabel('Magnitude of Error Misclassification on Test Set')
+plt.ylabel('Number of Occurences')
+plt.title('Number of Buckets SVM Was Off By on Test Set')
+plt.show()
+
+total_rec = sum(counts)
+zero_to_one_bucket = sum(counts[0:2])
+print("Percentage off by 1 bucket or less on test set: " + str( zero_to_one_bucket / total_rec) )
+
+
 Y_train_pred_rbf = rbf_clf.predict(X_train)
 train_perf_rbf = accuracy_score(Y_train, Y_train_pred_rbf)
 print("Linear performance on train set: " + str(train_perf) )
 print("RBF performance on train set: " + str(train_perf_rbf) )
+
 
 
 #Lets take a look at what's being predicted; i.e. where am I doing poorly?
